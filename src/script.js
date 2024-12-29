d4c65ea121a9fbe684fbf63b8362e0d133095b1e
DaniDela253 <danidavis321@gmail.com>
2024-12-29 11:37:16 -0600

    Update textures.

--- src/script.js
+++ src/script.js
@@ -4,9 +4,6 @@ import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 import GUI from "lil-gui";
 
-/**
- * Base
- */
 // Debug
 const gui = new GUI({
 	title: "Options",
@@ -20,21 +17,47 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load("textures/matcaps/8.png");
-matcapTexture.colorSpace = THREE.SRGBColorSpace;
+// Lights
+// const ambientLight = new THREE.AmbientLight(0xffffff, 5);
+const pointLight = new THREE.PointLight(0xffffff, 30);
+pointLight.position.x = 3;
+pointLight.position.x = 3;
+pointLight.position.x = 4;
+const ambientLight = new THREE.AmbientLight(0xffffff); // soft white light
+scene.add(ambientLight, pointLight);
 
-/**
- * Fonts
- */
+// Textures
+const textureLoader = new THREE.TextureLoader();
+// const matcapTexture = textureLoader.load("textures/matcaps/8.png");
+// matcapTexture.colorSpace = THREE.SRGBColorSpace;
+const colorTexture = textureLoader.load(
+	"./textures/ChristmasTreeOrnament/ChristmasTreeOrnament_Color.png"
+);
+colorTexture.colorSpace = THREE.SRGBColorSpace;
+const displacementTexture = textureLoader.load(
+	"./textures/ChristmasTreeOrnament/ChristmasTreeOrnament_Displacement.png"
+);
+// no height
+const normalTexture = textureLoader.load(
+	"./textures/ChristmasTreeOrnament/ChristmasTreeOrnament_NormalGL.png"
+);
+const roughnessTexture = textureLoader.load(
+	"./textures/ChristmasTreeOrnament/ChristmasTreeOrnament_Roughness.png"
+);
+const metalnessTexture = textureLoader.load(
+	"./textures/ChristmasTreeOrnament/ChristmasTreeOrnament_Metalness.png"
+);
+
+// Fonts
 const fontLoader = new FontLoader();
 
 fontLoader.load("/fonts/poly.json", (font) => {
 	// Material
-	const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+	// const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+	const material = new THREE.MeshStandardMaterial();
+	material.map = colorTexture;
+	material.metalnessMap = metalnessTexture;
+	material.roughnessMap = roughnessTexture;
 
 	// Text
 	const textGeometry = new TextGeometry("Merry Christmas", {
@@ -50,14 +73,14 @@ fontLoader.load("/fonts/poly.json", (font) => {
 	});
 	textGeometry.center();
 
-	const text = new THREE.Mesh(textGeometry, material);
+	const text = new THREE.Mesh(textGeometry, new THREE.MeshNormalMaterial());
 	scene.add(text);
 
 	const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
 	const sphereGeometry = new THREE.SphereGeometry(0.3, 30, 20);
 	const cubeGeometry = new THREE.BoxGeometry(0.3, 0.3, 0.3);
 
-	for (let i = 0; i < 100; i++) {
+	for (let i = 0; i < 75; i++) {
 		const donut = new THREE.Mesh(donutGeometry, material);
 		donut.position.x = (Math.random() - 0.5) * 15;
 		donut.position.y = (Math.random() - 0.5) * 15;
@@ -69,7 +92,7 @@ fontLoader.load("/fonts/poly.json", (font) => {
 
 		scene.add(donut);
 	}
-	for (let i = 0; i < 100; i++) {
+	for (let i = 0; i < 300; i++) {
 		const sphere = new THREE.Mesh(sphereGeometry, material);
 		sphere.position.x = (Math.random() - 0.5) * 15;
 		sphere.position.y = (Math.random() - 0.5) * 15;
@@ -81,7 +104,7 @@ fontLoader.load("/fonts/poly.json", (font) => {
 
 		scene.add(sphere);
 	}
-	for (let i = 0; i < 100; i++) {
+	for (let i = 0; i < 75; i++) {
 		const cube = new THREE.Mesh(cubeGeometry, material);
 		cube.position.x = (Math.random() - 0.5) * 15;
 		cube.position.y = (Math.random() - 0.5) * 15;
@@ -95,9 +118,7 @@ fontLoader.load("/fonts/poly.json", (font) => {
 	}
 });
 
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
 	width: window.innerWidth,
 	height: window.innerHeight,
@@ -117,9 +138,7 @@ window.addEventListener("resize", () => {
 	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-/**
- * Camera
- */
+// Camera
 // Base camera
 const camera = new THREE.PerspectiveCamera(
 	75,
@@ -136,18 +155,14 @@ scene.add(camera);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-/**
- * Renderer
- */
+// Render
 const renderer = new THREE.WebGLRenderer({
 	canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Animate
- */
+// Animate
 const clock = new THREE.Clock();
 
 const tick = () => {
